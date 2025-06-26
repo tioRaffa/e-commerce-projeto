4470f2cef3a61c5478efceb646bc6aff01ca5e30
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-26 11:44:44 -0300

    Refatorar UserViewSet: corrigir importação do modelo de perfil, ajustar métodos de atualização e corrigir erros de digitação

--- users/views/user_viewset.py
+++ users/views/user_viewset.py
@@ -3,18 +3,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
 from users.serializer import UserUpdateSerializer, ProfileUpdateSerializer, UserReadSerializer
-from models.profile import ProfileModel
+from users.models import ProfileModel
 
-class UserViewSet(viewsets.ModelViewSet):
-    def get_queryset(self):
-        return User.objects.filter(id=self.request.user.id)
+class UserViewSet(viewsets.GenericViewSet):
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == 'me' and self.request.method in ['PATCH']:
             return UserUpdateSerializer
         return UserReadSerializer
     
-    @action(detail=False, methods=['get', 'patch'], url_name='me')
+    @action(detail=False, methods=['get', 'patch'], url_path='me')
     def me(self, request, *args, **kwargs):
         user = self.request.user
 
@@ -22,21 +21,21 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer = self.get_serializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)   
 
-        if request.method == 'PATCH':
-            serializer = self.get_serializer(data=request.data, partial=True)
-            serializer.is_valid(raise_execptions=True)
-            validate_data = serializer.validated_data
+        elif request.method == 'PATCH':
+            serializer = self.get_serializer(instance=user, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            validated_data = serializer.validated_data
 
-            user.first_name = validate_data.get('fist_name', user.first_name)
-            user.last_name = validate_data.get('last_name', user.last_name)
-            user.save(update_files['first_name', 'last_name'])
+            user.first_name = validated_data.get('first_name', user.first_name)
+            user.last_name = validated_data.get('last_name', user.last_name)
+            user.save(update_fields=['first_name', 'last_name'])
 
-            profile_data = validate_data.get('profile')
+            profile_data = validated_data.get('profile')
             if profile_data:
                 profile, created = ProfileModel.objects.get_or_create(user=user)
 
                 profile_serializer = ProfileUpdateSerializer(
-                    isnstance=profile, data=profile_data, partial=True
+                    instance=profile, data=profile_data, partial=True
                 )
                 profile_serializer.is_valid(raise_exception=True)
                 profile_serializer.save()
