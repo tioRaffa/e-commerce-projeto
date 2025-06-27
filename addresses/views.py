f008d2fd95961e55fb25d1a8c15f7bd98f3b9e0d
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-27 12:41:20 -0300

    Adicionar verificação de limite de endereços no método create do AddressViewset

--- addresses/views.py
+++ addresses/views.py
@@ -1,8 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, permissions
 from .models import AddressModel
 from .serializer import AddressSerializer
 from common.permissions import IsOwenerAuth
-from rest_framework import permissions
+from rest_framework.response import Response
 
 class AddressViewset(viewsets.ModelViewSet):
     queryset = AddressModel.objects.all()
@@ -18,3 +18,15 @@ class AddressViewset(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        address_count = self.get_queryset().count()
+        if address_count >=3:
+            return Response({
+                "Detail": "Limite de 3 endereços por Usuario atingidos!"
+            },
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+        return super().create(request, *args, **kwargs)
