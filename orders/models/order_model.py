ac31ebb4b88271bb8b67a2d735c4be4fcc9558fc
tioRaffa <rafaelmuniz200@gmail.com>
2025-07-03 10:16:04 -0300

    Adicionar modelos de pedidos, incluindo campos para status, endereço de entrega e métodos de pagamento.

---
+++ orders/models/order_model.py
@@ -0,0 +1,94 @@
+from django.db import models
+from django.conf import settings
+from books.models.book_model import BookModel
+
+# Classe Base Corrigida
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+class OrderModel(Base):
+    class OrderStatus(models.TextChoices):
+        PENDING_PAYMENT = 'PENDING_PAYMENT', 'Aguardando Pagamento'
+        PROCESSING = 'PROCESSING', 'Processando'
+        SHIPPED = 'SHIPPED', 'Enviado'
+        DELIVERED = 'DELIVERED', 'Entregue'
+        CANCELED = 'CANCELED', 'Cancelado'
+        FAILED = 'FAILED', 'Falhou'
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Usuario',
+        related_name='orders'
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=OrderStatus.choices,
+        default=OrderStatus.PENDING_PAYMENT,
+        verbose_name='Status do Pedido'
+    )
+
+    # Endereço de entrega 
+    shipping_address = models.TextField(verbose_name="Endereço de Entrega Completo")
+
+    # Financeiro
+    total_items_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Preço Total dos Itens'
+    )
+    shipping_cost = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Custo do Frete'
+    )
+
+    # Melhor Envio
+    shipping_method = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name="Metodo de Envio (ex: SEDEX)"
+    )
+    tracking_code = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name="Codigo de Rastreio"
+    )
+    melhor_envio_order_id = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="ID do Pedido no Melhor Envio"
+    )
+
+    # Abacate Pay
+    payment_method = models.CharField(
+        max_length=50,
+        blank=True,
+        verbose_name='Metodo de Pagamento'
+    )
+    payment_charge_id = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        unique=True,
+        verbose_name='ID de transação -AbacatePay-'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+
+    def __str__(self):
+        return f"Pedido {self.id} - {self.user.username if self.user else 'Usuário Removido'}"
+
+    @property
+    def final_total(self):
+        return self.total_items_price + self.shipping_cost
+
