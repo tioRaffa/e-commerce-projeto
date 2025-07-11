9fe9622545d1db2295896b04d649600d803a0eef
tioRaffa <rafaelmuniz200@gmail.com>
2025-07-11 08:56:22 -0300

    Refatorar OrderAdmin para exibir e-mail do usuário e ajustar a ordenação. Adicionar lógica de exibição no OrderItemAdmin.

--- orders/admin.py
+++ orders/admin.py
@@ -29,6 +29,7 @@ class OrderAdmin(admin.ModelAdmin):
         'status', 'created_at'
     ]
     list_per_page = 10
+    ordering = ['-created_at']
     
 
 
@@ -39,7 +40,7 @@ class OrderAdmin(admin.ModelAdmin):
         return super().get_queryset(request).select_related('user', 'address')
 
     def user_display(self, obj):
-        return obj.user.first_name
+        return f'{obj.user.first_name} - {obj.user.email}'
     user_display.short_description = 'Usuario'
 
     def final_total_display(self, obj):
@@ -49,3 +50,36 @@ class OrderAdmin(admin.ModelAdmin):
     def address_zip_code(self, obj):
         return obj.address.zip_code
     address_zip_code.short_description = 'CEP'
+
+
+
+@admin.register(OrderItemModel)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'order_id',
+        'book',
+        'quantity',
+        'price_at_purchase',
+        'total_price'
+    ]
+    search_fields = [
+        'book__title', 'quantity', 'order__id'
+    ]
+    list_filter = [
+        'order__status',
+    ]
+    list_per_page = 10
+    ordering = ['-created_at']
+
+
+    def get_list_display_links(self, request, list_display):
+        return list_display
+
+    def total_price(self, obj):
+        return f'R$ {obj.total_price:,.2f}'.replace(",", "X").replace(".", ",").replace("X", ".")
+    total_price.short_description = 'Preço Total'
+
+    def order_id(self, obj):
+        return obj.order.id
+    order_id.short_description = 'Id-Order'
